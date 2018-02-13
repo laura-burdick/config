d6e08db8f734f30dde10b945ec435688d7423ce2
wenlaura <wenlaura@umich.edu>
2018-02-13 11:19:37 -0500

    htop and iftop

--- hacks.sh
+++ hacks.sh
@@ -8,6 +8,13 @@ top
 #See what processes are taking up computational power on a machine
 #(While top is running, press 1 to see CPU usage)
 
+htop
+#Fancier version of top
+#Press F6 (while htop is running) to sort by various parameters
+
+iftop
+#See network connections currently running on machine
+
 chmod -R 755 someDir/*
 #Recursive chmod
 
